04689fa5c49a284f798adb35c46c389a8f5ac11f
Taewan Kim <taewan.web.lab@gmail.com>
2023-03-22 16:28:06 +0900

    웹사이트 완성

--- src/pages/plans/Plans.jsx
+++ src/pages/plans/Plans.jsx
@@ -1,8 +1,37 @@
 import './plans.css';
+import {plans} from '../../data';
+import Header from '../../components/Header';
+import HeaderImage from '../../images/header_bg_4.jpg';
+import Card from '../../UI/Card';
 
 const Plans = () => {
+
   return (
-    <div>Plans</div>
+    <>
+      <Header title="Membership Plans" image={HeaderImage}>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat, sit?
+      </Header>
+      <section className='plans'>
+        <div className="container plans__container">
+          {
+            plans.map( ({id, name, desc, price, features}) => (
+              <Card key={id} className="plan">
+                <h3>{name}</h3>
+                <small>{desc}</small>
+                <h1>{`$${price}`}</h1><h2>/mo</h2>
+                <h4>Features</h4>
+                {
+                  features.map( ({feature, available}, index) => (
+                    <p key={index} className={!available ? 'disabled' : ''}>{feature}</p>
+                  ))
+                }
+                <button className='btn lg'>Choose Plan</button>
+              </Card>
+            ))
+          }
+        </div>
+      </section>
+    </>
   )
 }
 
--- src/pages/trainers/Trainers.jsx
+++ src/pages/trainers/Trainers.jsx
@@ -1,8 +1,36 @@
 import './trainers.css';
+import Header from '../../components/Header';
+import HeaderImage from '../../images/header_bg_5.jpg';
+import {trainers} from '../../data';
+import {BsInstagram} from 'react-icons/bs';
+import {AiOutlineTwitter} from 'react-icons/ai';
+import {FaFacebookF} from 'react-icons/fa';
+import { FaLinkedinIn } from 'react-icons/fa';
+import Trainer from '../../components/Trainer';
+
 
 const Trainers = () => {
+  console.log(trainers);
   return (
-    <div>Trainers</div>
+    <>
+      <Header title='Our Trainers' image={HeaderImage}>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati libero hic dignissimos labore est. Optio.
+      </Header>
+      <section className='trainers'>
+        <div className="container trainers__container">
+          {
+            trainers.map( ({id,image,name,job,socials}) => (
+              <Trainer key={id} image={image} name={name} job={job} socials={[
+                {icon: <BsInstagram />, link: socials[0]},
+                {icon: <AiOutlineTwitter />, link: socials[1]},
+                {icon: <FaFacebookF />, link: socials[2]},
+                {icon: <FaLinkedinIn />, link: socials[3]},
+              ]} />
+            ))
+          }
+        </div>
+      </section>
+    </>
   )
 }
 
