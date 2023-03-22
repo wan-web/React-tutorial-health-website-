fb1e703f6469f6d77eca9c36f0f992a82a21e8a2
Taewan Kim <taewan.web.lab@gmail.com>
2023-03-22 14:29:24 +0900

    add Gallery page

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,8 +1,26 @@
-import './contact.css'
+import './contact.css';
+import Header from '../../components/Header';
+import HeaderImage from '../../images/header_bg_2.jpg';
+import {MdEmail} from 'react-icons/md';
+import {BsMessenger} from 'react-icons/bs';
+import {IoLogoWhatsapp} from 'react-icons/io';
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <>
+      <Header title="Get In Touch" image={HeaderImage}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, ad aspernatur doloribus nostrum dignissimos cumque maxime nulla tempora libero. Aspernatur dolorem qui, fuga optio quae nesciunt eos id repudiandae modi.
+      </Header>
+      <section className="contact">
+        <div className="container contact__container">
+          <div className="contact__wrapper">
+            <a href="mailto:taewan.web.lab@gmail.com" target="_blank" rel="noreferrer noopener"><MdEmail /></a>
+            <a href="http://m.me/" target="_blank" rel="noreferrer noopener"><BsMessenger /></a>
+            <a href="https://wa.me/" target="_blank" rel="noreferrer noopener"><IoLogoWhatsapp /></a>
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
 
--- src/pages/gallery/Gallery.jsx
+++ src/pages/gallery/Gallery.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import './gallery.css';
+import Header from '../../components/Header';
+import HeaderImage from '../../images/header_bg_3.jpg';
 
 const Gallery = () => {
+  const galleryLength = 15;
+  const images = [];
+
+  for(let i = 0; i < galleryLength; i++) {
+    images.push(require(`../../images/gallery${i+1}.jpg`))
+  }
+
+
   return (
-    <div>Gallery</div>
+    <>
+      <Header title="Our Gallery" image={HeaderImage}>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis reprehenderit velit, voluptatibus eos quibusdam tempora.
+      </Header>
+      <section className="gallery">
+        <div className="container gallery__container">
+          {
+            images.map( (image, index) => (
+              <article key={index}>
+                <img src={image} alt={`Gallery ${index + 1}`} />
+              </article>
+            ))
+          }
+        </div>
+      </section>
+    </>
   )
 }
 
